14092f10b80f4073166ab0656b6c83fb2212b31e
Leonard Schüngel <schuengel.leonard@gmail.com>
2023-10-09 16:53:14 +0200

    Add random cars to the dumy data

--- unified_sim_model/src/games/dummy.rs
+++ unified_sim_model/src/games/dummy.rs
@@ -14,52 +14,6 @@ use crate::{
     AdapterError, Distance, GameAdapter, Temperature, UpdateEvent,
 };
 
-const FIRST_NAMES: [&str; 20] = [
-    "Liam",
-    "Noah",
-    "Oliver",
-    "James",
-    "Elijah",
-    "William",
-    "Henry",
-    "Lucas",
-    "Benjamin",
-    "Theodore",
-    "Mateo",
-    "Levi",
-    "Sebastian",
-    "Daniel",
-    "Jack",
-    "Michael",
-    "Alexander",
-    "Owen",
-    "Asher",
-    "Samuel",
-];
-
-const LAST_NAMES: [&str; 20] = [
-    "Smith",
-    "Johnson",
-    "Williams",
-    "Brown",
-    "Jones",
-    "Garcia",
-    "Miller",
-    "Davis",
-    "Rodriguez",
-    "Martinez",
-    "Hernandez",
-    "Lopez",
-    "Gonzalez",
-    "Wilson",
-    "Anderson",
-    "Thomas",
-    "Taylor",
-    "Moore",
-    "Jackson",
-    "Martin",
-];
-
 #[derive(Default)]
 pub struct DummyAdapter {}
 
@@ -131,12 +85,8 @@ impl GameAdapter for DummyAdapter {
                     },
                     current_driver: DriverId(0),
                     team_name: Value::new(format!("Team nr.{}", i)),
-                    car: Value::new(Car::new_static(
-                        "Car model",
-                        "Manufacturer",
-                        CarCategory { name: "Car Cat" },
-                    )),
-                    car_number: Value::new(rand.gen::<i32>() % 1000),
+                    car: Value::new(random_car()),
+                    car_number: Value::new(rand.gen::<i32>().abs() % 100),
                     nationality: Value::new(Nationality::NONE),
                     world_pos: Value::new([0.0, 0.0, 0.0]),
                     orientation: Value::new([0.0, 0.0, 0.0]),
@@ -154,13 +104,13 @@ impl GameAdapter for DummyAdapter {
                     best_lap: Value::new(None),
                     performance_delta: Value::new(Time::from(-1_234)),
                     time_behind_leader: Value::new(Time::from(12_345)),
-                    in_pits: Value::new(true),
+                    in_pits: Value::new(i % 3 == 0),
                     gear: Value::new(4),
                     speed: Value::new(128.0),
                     connected: Value::new(true),
                     stint_time: Value::new(Time::from(56_789)),
                     distance_driven: Value::new(i as f32 * 0.345),
-                    focused: i % 3 == 0,
+                    focused: i == 0,
                     game_data: EntryGameData::None,
                     is_finished: Value::new(false),
                 },
@@ -172,6 +122,51 @@ impl GameAdapter for DummyAdapter {
 }
 
 fn random_driver(id: DriverId) -> Driver {
+    const FIRST_NAMES: [&str; 20] = [
+        "Liam",
+        "Noah",
+        "Oliver",
+        "James",
+        "Elijah",
+        "William",
+        "Henry",
+        "Lucas",
+        "Benjamin",
+        "Theodore",
+        "Mateo",
+        "Levi",
+        "Sebastian",
+        "Daniel",
+        "Jack",
+        "Michael",
+        "Alexander",
+        "Owen",
+        "Asher",
+        "Samuel",
+    ];
+
+    const LAST_NAMES: [&str; 20] = [
+        "Smith",
+        "Johnson",
+        "Williams",
+        "Brown",
+        "Jones",
+        "Garcia",
+        "Miller",
+        "Davis",
+        "Rodriguez",
+        "Martinez",
+        "Hernandez",
+        "Lopez",
+        "Gonzalez",
+        "Wilson",
+        "Anderson",
+        "Thomas",
+        "Taylor",
+        "Moore",
+        "Jackson",
+        "Martin",
+    ];
     let mut rand = rand::thread_rng();
     let first_name = FIRST_NAMES[rand.gen::<usize>() % FIRST_NAMES.len()];
     let last_name = LAST_NAMES[rand.gen::<usize>() % LAST_NAMES.len()];
@@ -185,4 +180,64 @@ fn random_driver(id: DriverId) -> Driver {
         driving_time: Value::new(Time::from(0)),
         best_lap: Value::new(None),
     }
+}
+fn random_car() -> Car {
+    const GT3: CarCategory = CarCategory::new("GT3");
+    const GT4: CarCategory = CarCategory::new("GT4");
+    const ST: CarCategory = CarCategory::new("ST");
+    const ST22: CarCategory = CarCategory::new("ST");
+    const CUP: CarCategory = CarCategory::new("CUP");
+    const CUP21: CarCategory = CarCategory::new("CUP");
+    const CHL: CarCategory = CarCategory::new("CHL");
+    const TCX: CarCategory = CarCategory::new("TCX");
+    const CARS: [Car; 46] = [
+        Car::new_static("Porsche 991 GT3 R", "Porsche", GT3),
+        Car::new_static("Mercedes-AMG GT3", "Mercedes-AMG", GT3),
+        Car::new_static("Ferrari 488 GT3", "Ferrari", GT3),
+        Car::new_static("Audi R8 LMS", "Audi", GT3),
+        Car::new_static("Lamborghini Huracan GT3", "Lamborghini", GT3),
+        Car::new_static("McLaren 650S GT3", "McLaren", GT3),
+        Car::new_static("Nissan GT-R Nismo GT3 2018", "Nissan", GT3),
+        Car::new_static("BMW M6 GT3", "BMW", GT3),
+        Car::new_static("Bentley Continental GT3 2018", "Bentley", GT3),
+        Car::new_static("Porsche 991 II GT3 Cup", "Porsche", CUP),
+        Car::new_static("Nissan GT-R Nismo GT3", "Nissan", GT3),
+        Car::new_static("Bentley Continental GT3", "Bentley", GT3),
+        Car::new_static("AMR V12 Vantage GT3", "Aston-Martin", GT3),
+        Car::new_static("Reiter Engineering R-EX GT3", "Reiter-Engineering", GT3),
+        Car::new_static("Emil Frey Jaguar G3", "Jaguar", GT3),
+        Car::new_static("Lexus RC F GT3", "Lexus", GT3),
+        Car::new_static("Lamborghini Huracan GT3 Evo", "Lamborghini", GT3),
+        Car::new_static("Honda NSX GT3", "Honda", GT3),
+        Car::new_static("Lamborghini Huracan ST", "Lamborghini", ST),
+        Car::new_static("Audi R8 LMS Evo", "Audi", GT3),
+        Car::new_static("AMR V8 Vantage", "Aston-Martin", GT3),
+        Car::new_static("Honda NSX GT3 Evo", "Honda", GT3),
+        Car::new_static("McLaren 720S GT3", "McLaren", GT3),
+        Car::new_static("Porsche 911 II GT3 R", "Porsche", GT3),
+        Car::new_static("Ferrari 488 GT3 Evo", "Ferrari", GT3),
+        Car::new_static("Mercedes-AMG GT3 2020", "Mercedes-AMG", GT3),
+        Car::new_static("Ferrari 488 Challenge Evo", "Ferrari", CHL),
+        Car::new_static("BMW M2 CS Racing", "BMW", TCX),
+        Car::new_static("Porsche 992 GT3 CUP", "Porsche", CUP21),
+        Car::new_static("Lamborghini Huracan ST EVO2", "Lamborghini", ST22),
+        Car::new_static("BMW M4 GT3", "BMW", GT3),
+        Car::new_static("Audi R8 LMS Evo 2", "Audi", GT3),
+        Car::new_static("FERRARI 296 GT3", "Ferrari", GT3),
+        Car::new_static("Lamborghini Huracan EVO2", "Lamborghini", GT3),
+        Car::new_static("Porsche 992 GT3 R", "Porsche", GT3),
+        Car::new_static("Alpine A110 GT4", "Alpine", GT4),
+        Car::new_static("Aston Martin Vantage GT4", "Aston-Martin", GT4),
+        Car::new_static("Audi R8 LMS GT4", "Audi", GT4),
+        Car::new_static("BMW M4 GT4", "BMW", GT4),
+        Car::new_static("Chevrolet Camaro GT4", "Chevrolet", GT4),
+        Car::new_static("Ginetta G55 GT4", "Ginetta", GT4),
+        Car::new_static("KTM X-Bow GT4", "KTM", GT4),
+        Car::new_static("Maserati MC GT4", "Maserati", GT4),
+        Car::new_static("McLaren 570S GT4", "McLaren", GT4),
+        Car::new_static("Mercedes AMG GT4", "Mercedes-AMG", GT4),
+        Car::new_static("Porsche 718 Cayman GT4 Clubsport", "Porsche", GT4),
+    ];
+    let mut rand = rand::thread_rng();
+    CARS[rand.gen::<usize>() % CARS.len()].clone()
 }
