1d8bd9c6e2ac07ba21cdb4fe880ba19c38f56183
Leonard Schüngel <schuengel.leonard@gmail.com>
2023-12-26 15:23:09 +0100

    Add command change focused in the dummy adapter

--- unified_sim_model/src/games/dummy.rs
+++ unified_sim_model/src/games/dummy.rs
@@ -1,9 +1,15 @@
 use std::{
     collections::HashMap,
-    sync::{mpsc, Arc, RwLock},
+    ops::ControlFlow,
+    sync::{
+        mpsc::{self, TryRecvError},
+        Arc, RwLock,
+    },
+    time::Duration,
 };
 
 use rand::Rng;
+use tracing::error;
 
 use crate::{
     model::{
@@ -11,11 +17,15 @@ use crate::{
         Model, Nationality, Session, SessionGameData, SessionId, SessionPhase, SessionType, Value,
     },
     time::Time,
-    AdapterError, Distance, GameAdapter, Temperature, UpdateEvent,
+    AdapterCommand, AdapterError, Distance, GameAdapter, GameAdapterCommand, Temperature,
+    UpdateEvent,
 };
 
 /// Commands for the dummy adapter.
-pub enum DummyCommands {}
+pub enum DummyCommands {
+    /// Set the focus to a specific entry.
+    SetFocus(EntryId),
+}
 
 #[derive(Default)]
 pub struct DummyAdapter {}
@@ -24,103 +34,154 @@ impl GameAdapter for DummyAdapter {
     fn run(
         &mut self,
         model: Arc<RwLock<Model>>,
-        _command_rx: mpsc::Receiver<crate::AdapterCommand>,
-        _update_event: UpdateEvent,
+        command_rx: mpsc::Receiver<crate::AdapterCommand>,
+        update_event: UpdateEvent,
     ) -> Result<(), AdapterError> {
-        let mut model = model.write().expect("Should be able to lock for writing");
+        setup_model(&model);
 
-        model.event_name.set("Dummy event".to_string());
-        model.active_camera.set(Camera::Hellicopter);
-        model.available_cameras.insert(Camera::Hellicopter);
-        model.available_cameras.insert(Camera::Chase);
-        model.available_cameras.insert(Camera::FirstPerson);
-        model.focused_entry = None;
+        loop {
+            match command_rx.try_recv() {
+                Ok(AdapterCommand::Close) => break,
+                Ok(action) => {
+                    if self.handle_command(&model, action).is_break() {
+                        break;
+                    }
+                }
+                Err(TryRecvError::Empty) => (),
+                Err(TryRecvError::Disconnected) => {
+                    // This should only happen if all adapters have been dropped.
+                    // In which case it is impossible to interact with this adapter any more.
+                    // To avoid leaking memory we quit.
+                    error!("All adapter handle have been dropped it is impossible to communicate with this game adapter.");
+                    break;
+                }
+            };
 
-        // model.track_name = "Dummy track".to_string();
-        // model.track_length = 1234;
-
-        let id = model.add_session(Session {
-            id: SessionId(0),
-            entries: HashMap::new(),
-            session_type: Value::new(SessionType::Race),
-            session_time: Value::new(Time::from(1_200_123)),
-            time_remaining: Value::new(Time::from(754_123)),
-            laps: Value::new(20),
-            laps_remaining: Value::new(12),
-            phase: Value::new(SessionPhase::Active),
-            time_of_day: Value::new(Time::from(50_846_123)),
-            day: Value::new(Day::Sunday),
-            ambient_temp: Value::new(Temperature::from_celcius(24.0)),
-            track_temp: Value::new(Temperature::from_celcius(26.0)),
-            best_lap: Value::new(Some(Lap {
-                time: Value::new(Time::from(81_1234)),
-                splits: Value::new(vec![
-                    Time::from(12_345),
-                    Time::from(67_891),
-                    Time::from(111_213),
-                ]),
-                driver_id: Some(DriverId::default()),
-                entry_id: Some(EntryId::default()),
-                invalid: Value::new(false),
-            })),
-            track_name: Value::new("Dummy track".to_string()),
-            track_length: Value::new(Distance::from_meter(1234.0)),
-            game_data: SessionGameData::None,
-        });
-        model.current_session = Some(id);
-        model.events.push(Event::SessionChanged(SessionId(0)));
-
-        let mut rand = rand::thread_rng();
-        for i in 0..10 {
-            let session = model.current_session_mut().unwrap();
-            let entry_id = EntryId(i);
-            session.entries.insert(
-                entry_id,
-                Entry {
-                    id: entry_id,
-                    drivers: {
-                        let mut drivers = HashMap::new();
-                        for j in 0..3 {
-                            let driver_id = DriverId(j);
-                            drivers.insert(driver_id, random_driver(driver_id));
-                        }
-                        drivers
-                    },
-                    current_driver: DriverId(0),
-                    team_name: Value::new(format!("Team nr.{}", i)),
-                    car: Value::new(random_car()),
-                    car_number: Value::new(rand.gen::<i32>().abs() % 100),
-                    nationality: Value::new(Nationality::NONE),
-                    world_pos: Value::new([0.0, 0.0, 0.0]),
-                    orientation: Value::new([0.0, 0.0, 0.0]),
-                    position: Value::new(i + 1),
-                    spline_pos: Value::new(0.1234),
-                    lap_count: Value::new(0),
-                    laps: Vec::new(),
-                    current_lap: Value::new(Lap {
-                        time: Value::new(Time::from(12_345)),
-                        splits: Value::new(Vec::new()),
-                        driver_id: Some(DriverId(0)),
-                        entry_id: Some(EntryId(i)),
-                        invalid: Value::new(i % 2 == 0),
-                    }),
-                    best_lap: Value::new(None),
-                    performance_delta: Value::new(Time::from(-1_234)),
-                    time_behind_leader: Value::new(Time::from(12_345)),
-                    in_pits: Value::new(i % 3 == 0),
-                    gear: Value::new(4),
-                    speed: Value::new(128.0),
-                    connected: Value::new(true),
-                    stint_time: Value::new(Time::from(56_789)),
-                    distance_driven: Value::new(i as f32 * 0.345),
-                    focused: i == 0,
-                    game_data: EntryGameData::None,
-                    is_finished: Value::new(false),
-                },
-            );
+            update_event.trigger();
+            std::thread::sleep(Duration::from_millis(16));
         }
 
         Ok(())
+    }
+}
+impl DummyAdapter {
+    fn handle_command(
+        &mut self,
+        model: &Arc<RwLock<Model>>,
+        command: AdapterCommand,
+    ) -> ControlFlow<()> {
+        let mut model = model.write().expect("Should be able to lock for writing");
+        match command {
+            AdapterCommand::Close => return ControlFlow::Break(()),
+            AdapterCommand::Game(GameAdapterCommand::Dummy(command)) => match command {
+                DummyCommands::SetFocus(entry_id) => {
+                    model.focused_entry = Some(entry_id);
+                    if let Some(session) = model.current_session_mut() {
+                        session
+                            .entries
+                            .values_mut()
+                            .for_each(|entry| entry.focused = entry.id == entry_id);
+                    }
+                }
+            },
+            _ => (),
+        }
+        ControlFlow::Continue(())
+    }
+}
+
+fn setup_model(model: &Arc<RwLock<Model>>) {
+    let mut model = model.write().expect("Should be able to lock for writing");
+
+    model.event_name.set("Dummy event".to_string());
+    model.active_camera.set(Camera::Hellicopter);
+    model.available_cameras.insert(Camera::Hellicopter);
+    model.available_cameras.insert(Camera::Chase);
+    model.available_cameras.insert(Camera::FirstPerson);
+    model.focused_entry = None;
+
+    // model.track_name = "Dummy track".to_string();
+    // model.track_length = 1234;
+
+    let id = model.add_session(Session {
+        id: SessionId(0),
+        entries: HashMap::new(),
+        session_type: Value::new(SessionType::Race),
+        session_time: Value::new(Time::from(1_200_123)),
+        time_remaining: Value::new(Time::from(754_123)),
+        laps: Value::new(20),
+        laps_remaining: Value::new(12),
+        phase: Value::new(SessionPhase::Active),
+        time_of_day: Value::new(Time::from(50_846_123)),
+        day: Value::new(Day::Sunday),
+        ambient_temp: Value::new(Temperature::from_celcius(24.0)),
+        track_temp: Value::new(Temperature::from_celcius(26.0)),
+        best_lap: Value::new(Some(Lap {
+            time: Value::new(Time::from(81_1234)),
+            splits: Value::new(vec![
+                Time::from(12_345),
+                Time::from(67_891),
+                Time::from(111_213),
+            ]),
+            driver_id: Some(DriverId::default()),
+            entry_id: Some(EntryId::default()),
+            invalid: Value::new(false),
+        })),
+        track_name: Value::new("Dummy track".to_string()),
+        track_length: Value::new(Distance::from_meter(1234.0)),
+        game_data: SessionGameData::None,
+    });
+    model.current_session = Some(id);
+    model.events.push(Event::SessionChanged(SessionId(0)));
+
+    let mut rand = rand::thread_rng();
+    for i in 0..10 {
+        let session = model.current_session_mut().unwrap();
+        let entry_id = EntryId(i);
+        session.entries.insert(
+            entry_id,
+            Entry {
+                id: entry_id,
+                drivers: {
+                    let mut drivers = HashMap::new();
+                    for j in 0..3 {
+                        let driver_id = DriverId(j);
+                        drivers.insert(driver_id, random_driver(driver_id));
+                    }
+                    drivers
+                },
+                current_driver: DriverId(0),
+                team_name: Value::new(format!("Team nr.{}", i)),
+                car: Value::new(random_car()),
+                car_number: Value::new(rand.gen::<i32>().abs() % 100),
+                nationality: Value::new(Nationality::NONE),
+                world_pos: Value::new([0.0, 0.0, 0.0]),
+                orientation: Value::new([0.0, 0.0, 0.0]),
+                position: Value::new(i + 1),
+                spline_pos: Value::new(0.1234),
+                lap_count: Value::new(0),
+                laps: Vec::new(),
+                current_lap: Value::new(Lap {
+                    time: Value::new(Time::from(12_345)),
+                    splits: Value::new(Vec::new()),
+                    driver_id: Some(DriverId(0)),
+                    entry_id: Some(EntryId(i)),
+                    invalid: Value::new(i % 2 == 0),
+                }),
+                best_lap: Value::new(None),
+                performance_delta: Value::new(Time::from(-1_234)),
+                time_behind_leader: Value::new(Time::from(12_345)),
+                in_pits: Value::new(i % 3 == 0),
+                gear: Value::new(4),
+                speed: Value::new(128.0),
+                connected: Value::new(true),
+                stint_time: Value::new(Time::from(56_789)),
+                distance_driven: Value::new(i as f32 * 0.345),
+                focused: i == 0,
+                game_data: EntryGameData::None,
+                is_finished: Value::new(false),
+            },
+        );
     }
 }
 
