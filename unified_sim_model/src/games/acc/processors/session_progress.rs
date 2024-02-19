b859fb602e8cc53b56c57623db06f8abce89b022
Leonard Schüngel <schuengel.leonard@gmail.com>
2024-02-19 17:47:42 +0100

    Implement the session processor for a race session

--- unified_sim_model/src/games/acc/processors/session_progress.rs
+++ unified_sim_model/src/games/acc/processors/session_progress.rs
@@ -1,7 +1,18 @@
-use std::collections::HashMap;
+//! This module deals with calculating the distance driven and the is_finished state
+//! for each entry.
+//!
+//! The distance driven is simply calculated by adding the spline position to
+//! the lap count. However, ACC has a bunch of inconsistencies that are unwanted and
+//! need to be fixed.
+//!
+//! This implementation tries to do this by keeping track of the current state for each
+//! entry and adjusting the calcultion base don the current state.
+//! This way we can hopefully make sure that these fields work as intended.
+
+use std::{cmp::Ordering, collections::HashMap};
 
 use crate::{
-    games::acc::data::RealtimeCarUpdate,
+    games::acc::data::{RealtimeCarUpdate, SessionUpdate},
     model::{Entry, EntryId, Event, ScoringType, Session, SessionPhase},
 };
 
@@ -22,20 +33,105 @@ impl AccProcessor for SessionProgressProcessor {
         update: &RealtimeCarUpdate,
         context: &mut AccProcessorContext,
     ) -> crate::games::acc::Result<()> {
+        let Some(session) = context.model.current_session_mut() else {
+            return Ok(());
+        };
+
         let entry_id = EntryId(update.car_id as i32);
         let entry_state = self
             .entries
             .entry(entry_id)
-            .or_insert(EntryState::PreSession);
+            .or_insert(match *session.phase {
+                SessionPhase::None
+                | SessionPhase::Waiting
+                | SessionPhase::Preparing
+                | SessionPhase::Formation => EntryState::PreSession,
+                SessionPhase::Active => EntryState::Active,
+                SessionPhase::Ending => EntryState::Ending,
+                SessionPhase::Finished => EntryState::Finished,
+            });
 
-        let Some(session) = context.model.current_session_mut() else {
-            return Ok(());
-        };
+        // For debugging
+        if let Some(entry) = session.entries.get_mut(&entry_id) {
+            entry.team_name.set(match entry_state {
+                EntryState::PreSession => String::from("PreSession"),
+                EntryState::ActiveButNotCrossedTheLine => {
+                    String::from("ActiveButNotCrossedTheLine")
+                }
+                EntryState::Active => String::from("Active"),
+                EntryState::Ending => String::from("Ending"),
+                EntryState::Finished => String::from("Finished"),
+                EntryState::PotentialyFinished => String::from("PotentialyFinished"),
+            });
+        }
 
         match session.session_type.scoring_type() {
             ScoringType::BestLapTime => entry_state.best_lap(entry_id, session),
             ScoringType::DistanceThenTime => entry_state.distance_then_time(entry_id, session),
         }
+
+        Ok(())
+    }
+
+    fn session_update(
+        &mut self,
+        _update: &SessionUpdate,
+        context: &mut AccProcessorContext,
+    ) -> crate::games::acc::Result<()> {
+        // Resolve the potentially finished state for entries.
+        // Sort them by distance driven and if the leader is potentially finished then they become
+        // an actuall finisher. all other entries are returned to the ending state.
+        let Some(session) = context.model.current_session_mut() else {
+            return Ok(());
+        };
+        let mut entries = session.entries.values().collect::<Vec<_>>();
+        entries.sort_by(|e1, e2| {
+            e2.distance_driven
+                .partial_cmp(&e1.distance_driven)
+                .unwrap_or(Ordering::Equal)
+        });
+
+        let has_first_just_place_finished = entries
+            .first()
+            .and_then(|first| self.entries.get(&first.id))
+            .is_some_and(|first_state| matches!(first_state, EntryState::PotentialyFinished));
+        if has_first_just_place_finished {
+            // In case of a tie we want to set all entries with the same distance driven to finished
+            // everyone else goes back to ending.
+            if let Some(finished_distance) = entries.first().map(|first| *first.distance_driven) {
+                for entry in entries {
+                    if let Some(entry_state) = self.entries.get_mut(&entry.id) {
+                        if matches!(entry_state, EntryState::PotentialyFinished) {
+                            if entry.distance_driven >= finished_distance {
+                                *entry_state = EntryState::Finished;
+                            } else {
+                                *entry_state = EntryState::Ending;
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            let has_first_place_finished = entries
+                .first()
+                .and_then(|first| self.entries.get(&first.id))
+                .is_some_and(|first_state| matches!(first_state, EntryState::Finished));
+
+            // Set all potential finishers to finished if the first place has also finished.
+            for entry in entries {
+                if let Some(entry_state) = self.entries.get_mut(&entry.id) {
+                    if matches!(entry_state, EntryState::PotentialyFinished) {
+                        if has_first_place_finished {
+                            *entry_state = EntryState::Finished;
+                        } else {
+                            *entry_state = EntryState::Ending;
+                        }
+                    }
+                }
+            }
+        }
+
+        {}
 
         Ok(())
     }
@@ -64,25 +160,14 @@ enum EntryState {
     Active,
     /// The enrty is in the process of finishing the session.
     Ending,
+    /// This entry might have finished the session but we are not sure yet.
+    PotentialyFinished,
     /// The entry has completed the session.
     Finished,
 }
 
 impl EntryState {
     fn best_lap(&mut self, entry_id: EntryId, session: &mut Session) {
-        // For debugging
-        if let Some(entry) = session.entries.get_mut(&entry_id) {
-            entry.team_name.set(match self {
-                EntryState::PreSession => String::from("PreSession"),
-                EntryState::ActiveButNotCrossedTheLine => {
-                    String::from("ActiveButNotCrossedTheLine")
-                }
-                EntryState::Active => String::from("Active"),
-                EntryState::Ending => String::from("Ending"),
-                EntryState::Finished => String::from("Finished"),
-            });
-        }
-
         let session_phase = *session.phase;
         let Some(entry) = session.entries.get_mut(&entry_id) else {
             return;
@@ -110,10 +195,8 @@ impl EntryState {
                     EntryState::PreSession
                     | EntryState::ActiveButNotCrossedTheLine
                     | EntryState::Active => {
-                        let distance = get_distance_driven(entry);
-                        entry.distance_driven.set(distance);
-                        entry.is_finished.set(false);
                         *self = EntryState::Ending;
+                        self.best_lap(entry_id, session);
                     }
                     EntryState::Ending => {
                         let new_distance = get_distance_driven(entry);
@@ -126,26 +209,100 @@ impl EntryState {
                             entry.is_finished.set(false);
                         }
                     }
-                    // Do nothing.
-                    EntryState::Finished => (),
+                    EntryState::PotentialyFinished => unreachable!("For a best lap session an entry cannot be in the potentially finished state"),
+                    // Do nothing
+                    EntryState::Finished => {
+                        entry.is_finished.set(true);
+                    },
                 }
             }
             SessionPhase::Finished => {
                 // Move all entries that are not finished to the finished state.
-                if !matches!(self, EntryState::Finished) {
-                    *self = EntryState::Finished;
-                    entry.is_finished.set(true);
-                }
+                *self = EntryState::Finished;
+                entry.is_finished.set(true);
             }
         }
     }
 
-    fn distance_then_time(&mut self, _entry_id: EntryId, _session: &mut Session) {}
+    fn distance_then_time(&mut self, entry_id: EntryId, session: &mut Session) {
+        let session_phase = *session.phase;
+        let Some(entry) = session.entries.get_mut(&entry_id) else {
+            return;
+        };
+
+        match session_phase {
+            SessionPhase::None
+            | SessionPhase::Waiting
+            | SessionPhase::Preparing
+            | SessionPhase::Formation => {
+                *self = EntryState::PreSession;
+                entry.distance_driven.set(0.0);
+                entry.is_finished.set(false);
+            }
+            SessionPhase::Active => match self {
+                EntryState::PreSession => {
+                    *self = EntryState::ActiveButNotCrossedTheLine;
+                    self.distance_then_time(entry_id, session);
+                }
+                EntryState::ActiveButNotCrossedTheLine => {
+                    if entry.spline_pos < 0.5 && !entry.in_pits.as_ref() {
+                        *self = EntryState::Active;
+                        self.distance_then_time(entry_id, session);
+                    } else {
+                        entry.distance_driven.set(0.0);
+                        entry.is_finished.set(false);
+                    }
+                }
+                EntryState::Active => {
+                    let new_distance = get_distance_driven(entry);
+                    entry.distance_driven.set(new_distance);
+                    entry.is_finished.set(false);
+                }
+                EntryState::Ending | EntryState::PotentialyFinished | EntryState::Finished => {
+                    unreachable!("No entry should be in the ending or finished state at this point")
+                }
+            },
+            SessionPhase::Ending => match self {
+                EntryState::PreSession
+                | EntryState::ActiveButNotCrossedTheLine
+                | EntryState::Active => {
+                    *self = EntryState::Ending;
+                    self.distance_then_time(entry_id, session);
+                }
+                EntryState::Ending => {
+                    let new_distance = get_distance_driven(entry);
+                    if entry.distance_driven.fract() > 0.95 && new_distance.fract() < 0.5 {
+                        // We cannot be sure that this entry has finished the race since the leader
+                        // of the race has to finish before any other entry. Therefore
+                        // this entry becomes potentially finished until we can figure that out.
+                        entry.distance_driven.set(new_distance.floor());
+                        entry.is_finished.set(false);
+                        *self = EntryState::PotentialyFinished;
+                    } else {
+                        entry.distance_driven.set(new_distance);
+                        entry.is_finished.set(false);
+                    }
+                }
+                EntryState::PotentialyFinished => {
+                    unreachable!("The potentially finished state should have been resolved by now")
+                }
+                // Do nothing.
+                EntryState::Finished => {
+                    entry.is_finished.set(true);
+                }
+            },
+            // Do nothing.
+            SessionPhase::Finished => {
+                *self = EntryState::Finished;
+                entry.is_finished.set(true);
+            }
+        }
+    }
 }
 
 fn get_distance_driven(entry: &mut Entry) -> f32 {
     let mut distance_driven = *entry.spline_pos + *entry.lap_count as f32;
-    if (entry.spline_pos > 0.95 || entry.spline_pos < 0.05) && !*entry.in_pits {
+    if entry.distance_driven.fract() > 0.95 || entry.distance_driven.fract() < 0.05 {
         let diff_to_last_update = distance_driven - *entry.distance_driven;
         if diff_to_last_update < -0.5 {
             distance_driven += 1.0;
